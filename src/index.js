1189fecdc9f102f0f1c533c0f2a528ae3f9ab903
oliverfont <oliverfont@protonmail.com>
2024-08-27 11:23:23 +0200

    netoyage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Modal from 'react-modal';
 import './index.css';
-
-Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
